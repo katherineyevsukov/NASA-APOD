f749fbb27aba47a5299e07cb4fbaecfa4a54a8ca
Kat <kyevsukov@gmail.com>
2021-08-29 11:05:13 -0400

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Apod from './components/ApodDisplay'
+import DateSelector from './components/DateSelector'
+import PastApod from "./components/PastApod";
+import styled from 'styled-components'
+import logo from './images/nasalogo.png'
+
+const HeaderWrapper = styled.header`
+    text-align: center;
+    margin: 4% 20% 5% 20%;
+
+    h1{
+  font-style: italic;
+  font-size: 7rem;
+  margin-bottom: 2%;
+  /* text-shadow: 2px 2px #4f4f4f; */
+  color: rgba(0,0,0,0.6);
+  text-shadow: 3px 3px 3px rgba(0,0,0,0.5);
+}
+
+img.logo{
+  height:100px;
+  width: 100px;
+}
+    
+`
 
 function App() {
+
+  const [today, setToday] = useState(true)
+  const [date, setDate] = useState('')
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderWrapper>
+        <h1>NASA <span><img className='logo' src={logo} alt='logo'></img></span>APOD</h1>
+        <h2>Discover the cosmos! Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.</h2>
+      </HeaderWrapper>
+      {today ? <Apod /> : <PastApod date={date} />}
+       <DateSelector setDate={setDate} setToday={setToday} />
     </div>
+    
   );
 }
 
---
+++ src/components/PastApod.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { BASE_URL, API_KEY } from '../constants/index'
+import { StyledApodDiv } from './ApodDisplay'
+
+
+export default function PastApod(props) {
+   const { date } = props
+   const[pastApod, setPastApod] = useState([])
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}?api_key=${API_KEY}&date=${date}`)
+        .then(res => {
+            console.log('data', res.data)
+            setPastApod(res.data)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [date])
+   
+    return (
+    
+        <StyledApodDiv>
+           <div className='description-container'>
+               {<h3>{pastApod.date}</h3>}
+               <h2>"{pastApod.title}"</h2>
+               <p className='description'>{pastApod.explanation}</p>
+               <p className='copyright'>{pastApod.copyright && `Copyright: ${pastApod.copyright}`}</p>
+            </div>
+            <div className='image-container'>
+               <img src={pastApod.url} alt='Apod'></img> 
+            </div>    
+       </StyledApodDiv>
+   ) 
+}
