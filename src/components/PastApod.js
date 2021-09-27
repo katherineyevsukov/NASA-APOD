6ce2f8a53a02584a8bd8ea8ba94bc7b4621f2125
Kat <kyevsukov@gmail.com>
2021-09-27 10:40:14 -0400

    fixed for videos

--- src/components/PastApod.js
+++ src/components/PastApod.js
@@ -1,36 +1,40 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
-import { BASE_URL, API_KEY } from '../constants/index'
-import { StyledApodDiv } from './ApodDisplay'
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { BASE_URL, API_KEY } from "../constants/index";
+import { StyledApodDiv } from "./ApodDisplay";
 
 export default function PastApod(props) {
-   const { date } = props
-   const[pastApod, setPastApod] = useState([])
+  const { date } = props;
+  const [pastApod, setPastApod] = useState([]);
 
-    useEffect(() => {
-        axios.get(`${BASE_URL}?api_key=${API_KEY}&date=${date}`)
-        .then(res => {
-            console.log('data', res.data)
-            setPastApod(res.data)
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }, [date])
-   
-    return (
-    
-        <StyledApodDiv>
-           <div className='description-container'>
-               {<h3>{pastApod.date}</h3>}
-               <h2>"{pastApod.title}"</h2>
-               <p className='description'>{pastApod.explanation}</p>
-               {<p className='copyright'>{pastApod.copyright && `Copyright: ${pastApod.copyright}`}</p>}
-            </div>
-            <div className='image-container'>
-               <img src={pastApod.url} alt='Apod'></img> 
-            </div>    
-       </StyledApodDiv>
-   ) 
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}?api_key=${API_KEY}&date=${date}`)
+      .then((res) => {
+        console.log("data", res.data);
+        setPastApod(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [date]);
+
+  return (
+    <StyledApodDiv>
+      <div className="description-container">
+        {<h3>{pastApod.date}</h3>}
+        <h2>"{pastApod.title}"</h2>
+        <p className="description">{pastApod.explanation}</p>
+        {
+          <p className="copyright">
+            {pastApod.copyright && `Copyright: ${pastApod.copyright}`}
+          </p>
+        }
+      </div>
+      <div className="image-container">
+        {pastApod.media_type === "video" ? <iframe title ="apod video" src={pastApod.url}></iframe> :
+        <img src={pastApod.url} alt="Apod"></img>}
+      </div>
+    </StyledApodDiv>
+  );
 }
